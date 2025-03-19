d4fb8335cf712991bca8d12e7e60e7dde636ea9b
Furkan Dursun <furkandursun3428@gmail.com>
2025-03-19 19:03:52 +0300

    fix playwright-webpack

--- playwright.config.js
+++ playwright.config.js
@@ -73,8 +73,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:8081',
+    command: 'npm run build && npm run start',
+    url: 'http://127.0.0.1:8080',
     reuseExistingServer: !process.env.CI,
   },
 })
